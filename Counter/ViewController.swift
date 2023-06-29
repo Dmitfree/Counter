a933d56bd990192a6fb42ebc0a20e50fdac1fc84
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-06-29 22:57:24 +0300

    Добавил элементы на экран, проверил работоспособность приложения

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countButton: UIButton!
+    @IBOutlet weak var countResultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonDidLoad(_ sender: Any) {
+    }
+    
 
 }
 
