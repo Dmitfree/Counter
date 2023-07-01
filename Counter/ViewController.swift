1114c647a47eeebde49343fc1ef328b240c61ccf
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-07-02 02:09:28 +0300

    Добавил дату в логи, но еще не разобрался с DateFormatter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -19,18 +19,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var countButton: UIButton!
     
     @IBOutlet weak var countResultLabel: UILabel!
-    var result = 0
     
-    let dateFormatter = DateFormatter()
-  //  dateFormatter.dateStyle = .short
-  //  dateFormatter.timeStyle = .short
+    var result = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         decreaseCountButton.tintColor = .red
         
         logWindow.text = "История изменений:\n"
+        
+  /*  let date = Date()
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "ru-Ru")
+        var dateString = dateFormatter.string(from: date)
+   
+   не понятно как использовать DateFormatter
+   */
+        
     }
+    
     @IBAction func buttonDidLoad(_ sender: Any) {
         result += 1
         countResultLabel.text = "Значение счетчика: \(result)"
@@ -39,24 +48,23 @@ class ViewController: UIViewController {
     @IBAction func decreaseButtonDidLoad(_ sender: Any) {
         if result == 0 {
             countResultLabel.text = "Значение счетчика: 0"
-            logWindow.text.append("Попытка уменьшить значение счётчика ниже 0\n")
+            logWindow.text.append("\(Date()) Попытка уменьшить значение счётчика ниже 0\n")
         } else {
             result -= 1
             countResultLabel.text = "Значение счетчика: \(result)"
-            logWindow.text.append("Значение уменьшено на -1\n")
+            logWindow.text.append("\(Date()) Значение уменьшено на -1\n")
         }
     }
     
     @IBAction func increaseButtonDidLoad(_ sender: Any) {
         result += 1
         countResultLabel.text = "Значение счетчика: \(result)"
-        logWindow.text.append("Значение увеличено на +1\n")
+        logWindow.text.append("\(Date()) Значение увеличено на +1\n")
     }
     
     @IBAction func clearButtonDidLoad(_ sender: Any) {
         result = 0
         countResultLabel.text = "0"
-        logWindow.text.append("Значение сброшено\n")
+        logWindow.text.append("\(Date()) Значение сброшено\n")
     }
 }
-
