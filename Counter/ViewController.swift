27a8e2680285d6d2b334bae06d6110b5efca2fba
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-07-01 22:36:22 +0300

    Добавил отображение событий в журнале, согласнозаданию

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -21,11 +21,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var countResultLabel: UILabel!
     var result = 0
     
+    let dateFormatter = DateFormatter()
+  //  dateFormatter.dateStyle = .short
+  //  dateFormatter.timeStyle = .short
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         decreaseCountButton.tintColor = .red
         
-        logWindow.text = "История изменений:"
+        logWindow.text = "История изменений:\n"
     }
     @IBAction func buttonDidLoad(_ sender: Any) {
         result += 1
@@ -35,20 +39,24 @@ class ViewController: UIViewController {
     @IBAction func decreaseButtonDidLoad(_ sender: Any) {
         if result == 0 {
             countResultLabel.text = "Значение счетчика: 0"
+            logWindow.text.append("Попытка уменьшить значение счётчика ниже 0\n")
         } else {
             result -= 1
             countResultLabel.text = "Значение счетчика: \(result)"
+            logWindow.text.append("Значение уменьшено на -1\n")
         }
     }
     
     @IBAction func increaseButtonDidLoad(_ sender: Any) {
         result += 1
         countResultLabel.text = "Значение счетчика: \(result)"
+        logWindow.text.append("Значение увеличено на +1\n")
     }
     
     @IBAction func clearButtonDidLoad(_ sender: Any) {
         result = 0
         countResultLabel.text = "0"
+        logWindow.text.append("Значение сброшено\n")
     }
 }
 
