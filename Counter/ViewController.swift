94d598edc85e8eed491aac7514750c4ae12e2ef9
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-07-03 22:57:51 +0300

    Исправлены названия, задан модификатор доступа private для переменных и функций, убрал лишние комментарии.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,33 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var logWindow: UITextView!
+    @IBOutlet weak private var logWindow: UITextView!
     
-    @IBOutlet weak var clearResultButton: UIButton!
+    @IBOutlet weak private var clearResultButton: UIButton!
     
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var minusButton: UIButton!
     
-    @IBOutlet weak var TapButton: UIButton!
+    @IBOutlet weak private var tapButton: UIButton!
     
-    @IBOutlet weak var countResultLabel: UILabel!
+    @IBOutlet weak private var countResultLabel: UILabel!
     
     var result = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         minusButton.tintColor = .red
-        
         logWindow.text = "История изменений:\n"
-    
     }
     
-    @IBAction func TapButtonDidLoad(_ sender: Any) {
+    @IBAction private func tapButtonDidLoad(_ sender: Any) {
         result += 1
         countResultLabel.text = "Значение счетчика: \(result)"
     }
     
-    @IBAction func minusButtonDidLoad(_ sender: Any) {
+    @IBAction private func minusButtonDidLoad(_ sender: Any) {
         if result == 0 {
             countResultLabel.text = "Значение счетчика: 0"
             logWindow.text.append("\(Date()) Попытка уменьшить значение счётчика ниже 0\n")
@@ -46,13 +44,13 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func plusButtonDidLoad(_ sender: Any) {
+    @IBAction private func plusButtonDidLoad(_ sender: Any) {
         result += 1
         countResultLabel.text = "Значение счетчика: \(result)"
         logWindow.text.append("\(Date()) Значение увеличено на +1\n")
     }
     
-    @IBAction func clearButtonDidLoad(_ sender: Any) {
+    @IBAction private func clearButtonDidLoad(_ sender: Any) {
         result = 0
         countResultLabel.text = "0"
         logWindow.text.append("\(Date()) Значение сброшено\n")
